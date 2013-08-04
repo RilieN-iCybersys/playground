ef7c974cd2e5560ff34f2de988a3cd626cb328b6
Ri|ieN <Rilien.iCybersys@gmail.com>
2013-08-04 11:08:40 +0800

    First commit

---
+++ src/newapp1/NewApp1.java
@@ -0,0 +1,12 @@
+package newapp1;
+import java.util.Scanner;
+
+public class NewApp1 {
+
+    public static void main(String[] args) {
+        Scanner cIn = new Scanner(System.in);
+        
+            
+        
+    }
+}
